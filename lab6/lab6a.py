0374e3ba298703b950b90550552d8a8dfb82a57d
Joar <joaek383@student.liu.se>
2022-11-03 12:58:55 +0100

    First commit

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -1,3 +1,44 @@
 """
 ConstCalc
 """
+from calc import *
+
+def exec_program(p: list()):
+    if is_program(p):
+        define_statements(program_statements(p))
+
+
+def define_statements(statements):
+    if statements == []:
+        return None
+    elif is_output(statements[0]):
+        print(output_expression(statements[0]))
+        return define_statements(rest_statements(statements))
+
+
+def if_statement(statement):
+    cond = selection_condition(statement)
+    if is_condition(cond):
+        if condition_operator(cond) == '<':
+            if condition_left(cond) < condition_right(cond):
+                return True
+            return False
+        elif condition_operator(cond) == '>':
+            if condition_left(cond) > condition_right(cond):
+                return True
+            return False
+        elif condition_operator(cond) == '=':
+            if condition_left(cond) == condition_right(cond):
+                return True
+            return False
+
+
+cond = ['if', [3, '>', 5], ['print', 2], ['print', 4]]
+
+print(if_statement(cond))
+
+calc1 = ['calc', ['print', 2], ['print', 4], ['print', 73]]
+
+exec_program(calc1)
+
+#def exec_statement(s: list())
