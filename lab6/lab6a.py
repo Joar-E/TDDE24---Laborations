fa25f735116f58ee662c667afe03c85b3d3ad818
Joar <joaek383@student.liu.se>
2022-11-04 16:34:47 +0100

    Added eval_expression and we are experimenting

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -6,39 +6,82 @@ from calc import *
 def exec_program(p: list()):
     if is_program(p):
         define_statements(program_statements(p))
+    else:
+        print("Is not Calc program")
+
+
+"""def exec_statement(statement):
+    if is_output(statement):
+        print(output_expression(statement))
+    elif is_binaryexpr(statement):
+        return None
 
 
 def define_statements(statements):
-    if statements == []:
-        return None
-    elif is_output(statements[0]):
-        print(output_expression(statements[0]))
-        return define_statements(rest_statements(statements))
+    for statement in statements:
 
+        if is_selection(statement):
+            exec_statement(eval_condition(statement))
+        else:
+            exec_statement(statement)"""
 
-def if_statement(statement):
+def eval_expression(statements):
+
+    if isinstance(statements, list):
+        if is_assignment(first_statement(statements)):
+            return None
+        elif is_repetition(first_statement(statements)):
+            return None
+        elif is_selection(first_statement(statements)):
+            print(eval_condition(first_statement(statements)))
+
+        elif is_output(first_statement(statements)):
+            ""
+        elif is_input(first_statement(statements)):
+            ""
+        elif is_binaryexpr(first_statement(statements)):
+            eval_binaryoper(first_statement(statements))
+    else:
+        return first_statement(statements)
+
+cond = selection_condition(['if', [3, '>', 5], ['print', 2], ['print', 4]])
+
+print(eval_expression(condition_left(cond)))
+
+def eval_condition(statement):
     cond = selection_condition(statement)
+
+    cond_left = eval_expression(condition_left(cond))
+    cond_right = eval_expression(condition_right(cond))
+
     if is_condition(cond):
-        if condition_operator(cond) == '<':
-            if condition_left(cond) < condition_right(cond):
-                return True
+        if condition_operator(cond) == '<' and (cond_left < cond_right):
+            return selection_true_branch(statement)
+
+        elif condition_operator(cond) == '>' and (cond_left > cond_right):
+            return selection_true_branch(statement)
+
+        elif condition_operator(cond) == '=' and (cond_left == cond_right):
+            return selection_true_branch(statement)
+
+        elif selection_has_false_branch(statement):
+            return selection_false_branch(statement)
+        
+        else:
             return False
-        elif condition_operator(cond) == '>':
-            if condition_left(cond) > condition_right(cond):
-                return True
-            return False
-        elif condition_operator(cond) == '=':
-            if condition_left(cond) == condition_right(cond):
-                return True
+
+def eval_binaryoper(statement):
+    binary = selection_condition(statement)
+    if is_binaryexpr(binary):
+        if bineryexp_operator(binary) == "+":
             return False
 
 
-cond = ['if', [3, '>', 5], ['print', 2], ['print', 4]]
 
-print(if_statement(cond))
+a = [['if', [3, '>', 5], ['print', 2], ['print', 4]]]
 
-calc1 = ['calc', ['print', 2], ['print', 4], ['print', 73]]
-
-exec_program(calc1)
-
-#def exec_statement(s: list())
+#eval_expression(a)
+#calc1 = [['print', 2], ['print', 4]]
+#calc2 = ['calc', ['if', [3, '>', 5], ['print', 2], ["print", 4]], ["print", 5]]
+#exec_program(calc2)
+#exec_program(calc1)
