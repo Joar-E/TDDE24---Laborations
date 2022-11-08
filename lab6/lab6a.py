685edf010b0ab5ec1d99ec603ab89c5dcd0432c4
Joar <joaek383@student.liu.se>
2022-11-08 16:55:21 +0100

    Klar 6a

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -5,48 +5,43 @@ from calc import *
 
 def exec_program(p: list()):
     if is_program(p):
-        define_statements(program_statements(p))
+        exec_statements(program_statements(p))
     else:
         print("Is not Calc program")
 
+variable_dict = {}        
 
-"""def exec_statement(statement):
-    if is_output(statement):
-        print(output_expression(statement))
+
+def exec_statements(statements, rest_list = False):
+    if not empty_statements(statements):
+        return (eval_expression(first_statement(statements)), exec_statements(rest_statements(statements)))
+
+
+def eval_expression(statement):
+    if is_statement(statement):
+        return eval_statement(statement)
     elif is_binaryexpr(statement):
-        return None
+        return eval_binaryoper(statement)
+    elif is_variable(statement):
+        return eval_variable(statement)
+    elif is_constant(statement):
+        return(statement)
 
 
-def define_statements(statements):
-    for statement in statements:
+def eval_statement(s):
+    if is_assignment(s):
+        return eval_assignment(s)
 
-        if is_selection(statement):
-            exec_statement(eval_condition(statement))
-        else:
-            exec_statement(statement)"""
+    elif is_selection(s):
+        return eval_condition(s)
 
-def eval_expression(statements):
+    elif is_output(s):
+        return eval_output(s)
 
-    if isinstance(statements, list):
-        if is_assignment(first_statement(statements)):
-            return None
-        elif is_repetition(first_statement(statements)):
-            return None
-        elif is_selection(first_statement(statements)):
-            print(eval_condition(first_statement(statements)))
 
-        elif is_output(first_statement(statements)):
-            ""
-        elif is_input(first_statement(statements)):
-            ""
-        elif is_binaryexpr(first_statement(statements)):
-            eval_binaryoper(first_statement(statements))
-    else:
-        return first_statement(statements)
+def eval_assignment(a):
+    variable_dict[assignment_variable(a)] = eval_expression(assignment_expression(a))
 
-cond = selection_condition(['if', [3, '>', 5], ['print', 2], ['print', 4]])
-
-print(eval_expression(condition_left(cond)))
 
 def eval_condition(statement):
     cond = selection_condition(statement)
@@ -56,32 +51,46 @@ def eval_condition(statement):
 
     if is_condition(cond):
         if condition_operator(cond) == '<' and (cond_left < cond_right):
-            return selection_true_branch(statement)
+            return eval_expression(selection_true_branch(statement))
 
         elif condition_operator(cond) == '>' and (cond_left > cond_right):
-            return selection_true_branch(statement)
+            return eval_expression(selection_true_branch(statement))
 
         elif condition_operator(cond) == '=' and (cond_left == cond_right):
-            return selection_true_branch(statement)
+            return eval_expression(selection_true_branch(statement))
 
         elif selection_has_false_branch(statement):
-            return selection_false_branch(statement)
+            return eval_expression(selection_false_branch(statement))
         
         else:
             return False
 
-def eval_binaryoper(statement):
-    binary = selection_condition(statement)
-    if is_binaryexpr(binary):
-        if bineryexp_operator(binary) == "+":
-            return False
+
+def eval_output(o):
+    return print(eval_expression(output_expression(o)))
+
+
+def eval_binaryoper(b):
+
+    biny_left = eval_expression(binaryexpr_left(b))
+    biny_right = eval_expression(binaryexpr_right(b))
+
+    if binaryexpr_operator(b) == "+":
+        return (biny_left + biny_right)
+    elif binaryexpr_operator(b) == "-":
+        return (biny_left - biny_right)
+    elif binaryexpr_operator(b) == "/":
+        return (biny_left / biny_right)
+    elif binaryexpr_operator(b) == "*":
+        return (biny_left * biny_right)
+
+
+def eval_variable(v):
+    if v in variable_dict:
+        return variable_dict[v]
 
 
 
-a = [['if', [3, '>', 5], ['print', 2], ['print', 4]]]
-
-#eval_expression(a)
-#calc1 = [['print', 2], ['print', 4]]
-#calc2 = ['calc', ['if', [3, '>', 5], ['print', 2], ["print", 4]], ["print", 5]]
-#exec_program(calc2)
-#exec_program(calc1)
+calc1 = ['calc', ["set", "res", 5], ['print', 2], ['print', [4, "+", [2, "*", 2]]], ["print", "res"]]
+calc2 = ['calc', ['if', [[3, "+", 2], '<', 5], ['print', 2], ['print', 4]]]
+exec_program(calc2)
