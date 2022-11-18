c2e5c15a0026425540ba45b73239956665a62650
Joar <joaek383@student.liu.se>
2022-11-18 16:19:56 +0100

    Finished product

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -4,32 +4,38 @@ ConstCalc
 from calc import *
 
 def exec_program(p, table = {}):
+    """Runs the program if it is a Calc program"""
     if is_program(p):
         return exec_statements(program_statements(p), table)
     else:
-        print("Is not Calc program")
+        raise SyntaxError("Is not Calc program")
 
 
 def exec_statements(statements, table):
+    """Runs through every statement, one at a time"""
     if not empty_statements(statements):
         new_table = eval_expression(first_statement(statements), table)
         return exec_statements(rest_statements(statements), new_table)
     else:
         return table
 
-def eval_expression(statement, table):
-    if is_statement(statement):
-        return eval_statement(statement, table)
-    elif is_binaryexpr(statement):
-        return eval_binaryoper(statement, table)
-    elif is_variable(statement):
-        return eval_variable(statement, table)
-    elif is_constant(statement):
-        return statement
+
+def eval_expression(expression, table):
+    """Checks what type of expression it is and evaluates it"""
+    if is_statement(expression):
+        return eval_statement(expression, table)
+    elif is_binaryexpr(expression):
+        return eval_binaryoper(expression, table)
+    elif is_variable(expression):
+        return eval_variable(expression, table)
+    elif is_constant(expression):
+        return expression
     else:
-        raise ValueError('Input was wrong')
+        raise SyntaxError('Calc program syntax was invalid')
+
 
 def eval_statement(s, table):
+    """Checks the type of statement and executes it"""
     if is_assignment(s):
         return exec_assignment(s, table)
 
@@ -46,141 +52,104 @@ def eval_statement(s, table):
         return exec_output(s, table)
 
 
-def exec_assignment(a, table):
+def exec_assignment(s, table):
+    """Executes set statements"""
     table = table.copy()
 
-    table[assignment_variable(a)] = eval_expression(assignment_expression(a), table)
+    table[assignment_variable(s)] = eval_expression(assignment_expression(s), table)
     
     return table
 
 
-def exec_repetition(r, table):
-    table = table.copy()
-
-    while eval_condition(r, table):
-        table = exec_statements(repetition_statements(r), table)
+def exec_repetition(s, table):
+    """Executes while statements"""
+    while eval_condition(s, table):
+        table = exec_statements(repetition_statements(s), table)
     return table
 
 
-def exec_selection(statement, table):
-    if eval_condition(statement, table):
-         return eval_expression(selection_true_branch(statement), table)
+def exec_selection(s, table):
+    """Executes if statements"""
+    if eval_condition(s, table):
+         return eval_expression(selection_true_branch(s), table)
 
-    elif selection_has_false_branch(statement):
-         return eval_expression(selection_false_branch(statement), table)
+    elif selection_has_false_branch(s):
+         return eval_expression(selection_false_branch(s), table)
     return table
 
 
-def exec_input(i, table):
+def exec_input(s, table):
+    """Executes read statements"""
     table = table.copy()
     
-    user_input = int(input(f'Enter value for {input_variable(i)}: '))
+    user_input = int(input(f'Enter value for {input_variable(s)}: '))
 
-    table[input_variable(i)] = eval_expression(user_input, table)
+    table[input_variable(s)] = eval_expression(user_input, table)
     
     return table
 
 
-def exec_output(o, table):
-    output = eval_expression(output_expression(o), table)
+def exec_output(s, table):
+    """Executes print statements"""
+    output = eval_expression(output_expression(s), table)
 
-    if is_variable(output_expression(o)):
-        print(f'{output_expression(o)} = {eval_variable(output_expression(o), table)}')
+    if is_variable(output_expression(s)):
+        print(f'{output_expression(s)} = {eval_variable(output_expression(s), table)}')
     else:
         print(output)
 
     return table
 
 
-# def eval_condition(statement, table):
-#     cond = selection_condition(statement)
-
-#     cond_left = eval_expression(condition_left(cond), table)
-#     cond_right = eval_expression(condition_right(cond), table)
-
-#     if is_condition(cond):
-#         if condition_operator(cond) == '<':
-#             return cond_left < cond_right
-
-#         elif condition_operator(cond) == '>':
-#             return cond_left > cond_right
-
-#         elif condition_operator(cond) == '=':
-#             return cond_left == cond_right 
-
-#         else:
-#             return False
-
-
-def eval_condition(statement, table):
-    cond = selection_condition(statement)
+def eval_condition(s, table):
+    """Evaluates the condition inside of the statement and 
+       returns whether it is True or False"""
+    cond = selection_condition(s)
 
     cond_left = eval_expression(condition_left(cond), table)
     cond_right = eval_expression(condition_right(cond), table)
 
-    condopers = {'<': lambda x, y: x < y, '>': lambda x, y: x > y, '=': lambda x, y: x == y}
+    condopers = {'<': lambda x, y: x < y, '>': lambda x, y: x > y,\
+                 '=': lambda x, y: x == y}
 
     if is_condition(cond):
         if condition_operator(cond) in condopers:
             return condopers[condition_operator(cond)](cond_left, cond_right)
 
 
+def eval_binaryoper(s, table):
+    """Evaluates the binaryexpression inside of the statements
+       and executes the calculation"""
+    biny_left = eval_expression(binaryexpr_left(s), table)
+    biny_right = eval_expression(binaryexpr_right(s), table)
 
-def eval_binaryoper(b, table):
+    op = {'+': lambda x, y: x + y, '-': lambda x, y: x - y, \
+          '*': lambda x, y: x * y, '/': lambda x, y: x / y}
 
-    biny_left = eval_expression(binaryexpr_left(b), table)
-    biny_right = eval_expression(binaryexpr_right(b), table)
-    
-    if binaryexpr_operator(b) == "+":
-        return (biny_left + biny_right)
-    elif binaryexpr_operator(b) == "-":
-        return (biny_left - biny_right)
-    elif binaryexpr_operator(b) == "/":
-        return (biny_left / biny_right)
-    elif binaryexpr_operator(b) == "*":
-        return (biny_left * biny_right)
-    print('Can not compute binaryoperator')
-    #return table
+    if binaryexpr_operator(s) in op:
+        return op[binaryexpr_operator(s)](biny_left, biny_right)
 
 
-# def eval_binaryoper(b, table):
-
-#     biny_left = eval_expression(binaryexpr_left(b), table)
-#     biny_right = eval_expression(binaryexpr_right(b), table)
-
-#     op = ['+', '-', '*', '/']
-    
-#     if binaryexpr_operator(b) in op:
-#         return eval(f'{biny_left} {binaryexpr_operator(b)} {biny_right}')
-
-
-# def eval_binaryoper(b, table):
-#     biny_left = eval_expression(binaryexpr_left(b), table)
-#     biny_right = eval_expression(binaryexpr_right(b), table)
-
-#     op = {'+': lambda x, y: x + y, '-': lambda x, y: x - y, '*': lambda x, y: x * y, '/': lambda x, y: x / y}
-
-#     if binaryexpr_operator(b) in op:
-#         return op[binaryexpr_operator(b)](biny_left, biny_right)
-
-
-def eval_variable(v, table):
-    return table[v]
+def eval_variable(s, table):
+    """Returns the value of a given key in the given table"""
+    return table[s]
 
 
 # --- Correct statements ---
-calc_set_print = ['calc', ["set", "res", 5], ['print', 2], ['print', [4, "+", [2, "/", 2]]], ["print", "res"]]
+# calc_set_print = ['calc', ["set", "res", 5], ['print', 2], ['print', [4, "+", [2, "*", 2]]], ["print", "res"]]
 
-calc_if = ['calc', ['if', [[3, "+", ], '=', 6], ['print', True], ["print", False]]]
+# calc_if = ['calc', ['if', [[3, "+", 3], '=', 6], ['print', True], ["print", False]]]
 
-calc_read_print = ['calc', ['read', 'n'], ['print', 'n']]
+# calc_read_print = ['calc', ['read', 'n'], ['print', 'n']]
 
-calc_fac = ['calc', ['read', 'n'], ['set', 'sum', 1], ['while', ['n', '>', 0], ['set', 'sum', ['sum', '*', 'n']], ['set', 'n', ['n', '-', 1]]], ['print', 'sum']]
+# calc_fac = ['calc', ['read', 'n'], ['set', 'sum', 1], ['while', ['n', '>', 0], ['set', 'sum', ['sum', '*', 'n']], ['set', 'n', ['n', '-', 1]]], ['print', 'sum']]
 
-calc_fib = ['calc', ['read', 'steps'], ['set', 'n', 1], ['set', 'n0', 0], ['while', ['steps', '>', 0], ['set', 'n', ['n', '+', 'n0']], ['set', 'n0', ['n', '-', 'n0']], ['print', 'n'], ['set', 'steps', ['steps', '-', 1]]]]
+# calc_fib = ['calc', ['read', 'steps'], ['set', 'n', 1], ['set', 'n0', 0], ['while', ['steps', '>', 0], ['set', 'n', ['n', '+', 'n0']], ['set', 'n0', ['n', '-', 'n0']], ['print', 'n'], ['set', 'steps', ['steps', '-', 1]]]]
 
 
-# --- Faulty statements ---
-calc_error_binary = ['calc', ['read', 'n'], ['set', 'n', ['n', '%', 2]], ['print', 'n']]
+# # --- Faulty statements ---
+# calc_error_binary = ['calc', ['read', 'n'], ['set', 'n', ['n', '%', 2]], ['print', 'n']]
 
-exec_program(calc_if)
+# calc_error_not_calc = ['hej', ['print', 5]]
+
+# exec_program(calc_error_not_calc)
