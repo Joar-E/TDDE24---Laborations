7979559103760337c0611f4f3f02b1ad89d3dff9
Lucas <lucfa713@student.liu.se>
2022-11-22 17:02:49 +0100

    Skapar tester och gjort tree_size

--- lab7/lab7b.py
+++ lab7/lab7b.py
@@ -6,7 +6,7 @@ def is_empty_tree(tree):
 def is_leaf(tree):
     return isinstance(tree, int)
 
-def is_node(tree):
+def is_inner_node(tree):
     if len(tree) == 3 and isinstance(left_subtree(tree), (list, int)) and isinstance(right_subtree(tree), (list, int)):
         return True
     return False
@@ -26,25 +26,6 @@ def right_subtree(tree):
 
 # ----------------------------------------------------------------------
 
-# def empty_tree_fn():
-#     return 0
-
-# def leaf_fn(leaf_key):
-#     return leaf_key**2
-
-# def inner_node_fn(inner_node_key, left_value, right_value):
-#     return inner_node_key + left_value
-
-"""Huvudfunktion"""
-# def traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn):
-#     if is_empty_tree(tree):
-#         return empty_tree_fn()
-#     elif isinstance(left_subtree(tree), list):
-#         return inner_node_fn(node(tree), leaf_fn(traverse(left_subtree(tree), inner_node_fn, leaf_fn, empty_tree_fn)), right_subtree(tree))
-
-#     else:
-#         return inner_node_fn(node(tree), leaf_fn(left_subtree(tree)), right_subtree(tree))
-
 def traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn):
     if is_empty_tree(tree):
         return empty_tree_fn()
@@ -52,7 +33,7 @@ def traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn):
     elif is_leaf(tree):
         return leaf_fn(tree)
     
-    elif is_node(tree):
+    elif is_inner_node(tree):
         left_branch = traverse(left_subtree(tree), inner_node_fn, leaf_fn, empty_tree_fn)
 
         right_branch = traverse(right_subtree(tree), inner_node_fn, leaf_fn, empty_tree_fn)
@@ -76,3 +57,64 @@ def contains_key(search_value, tree):
         return False
     
     return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
+    
+
+def tree_size(tree):
+    sum = 0
+    
+    def inner_node_fn(tree, left_branch, right_branch):
+        nonlocal sum
+        sum += 1
+        return sum
+
+    def leaf_fn(tree):
+        nonlocal sum
+        sum += 1
+        return sum
+
+    def empty_tree_fn():
+        return sum
+
+    return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
+
+
+def tree_depth(tree):
+    depth = 1
+
+    def inner_node_fn(tree, left_branch, right_branch):
+        nonlocal depth
+        depth += 1
+        return depth
+
+    def leaf_fn(tree):
+        return depth
+
+    def empty_tree_fn():
+        return depth
+
+    return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
+
+if __name__ == '__main__':
+
+    """Tests for the contains_key function"""
+    assert contains_key(2, [1, 2, 3])
+    assert contains_key(3, [[], 4, 3])
+    assert contains_key(5, [[3, 7, []], 4, [[1, 2, 3], 8, [5, 6, []]]])
+
+    assert contains_key(5, [1, 2, 3]) == False
+    assert contains_key(93, [[], 4, 3]) == False
+    assert contains_key(9, [[3, 7, []], 4, [[1, 2, 3], 8, [5, 6, []]]]) == False
+
+    print("Contains_key past all tests")
+
+#-----------------------------------------------------------------
+
+    """Tests for the tree_size function"""
+    assert tree_size([1, 3, []]) == 2
+    assert tree_size([2, 4, 8]) == 3
+    assert tree_size([[1, 2, []], 3, []]) == 3
+    assert tree_size([[[1, 6, 1], 3, []], 1, [[1, 1, 1], 5, [6, 5, 5]]]) == 12
+
+    print("Tree_size passed all the tests")
+
+#-----------------------------------------------------------------
