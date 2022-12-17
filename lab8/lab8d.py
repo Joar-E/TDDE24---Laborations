4be3309aa1563a82f485304045ef1ee40f5ff171
Joar <joaek383@student.liu.se>
2022-12-17 17:22:16 +0100

    Troligtvis klar med funktionen free_spans

--- lab8/lab8d.py
+++ lab8/lab8d.py
@@ -17,10 +17,56 @@ else:
 
 
 def free_spans(cal_day: CalendarDay, start: Time, end: Time) -> TimeSpanSeq:
-    time_spans_seq = new_time_span_seq()
+    app_spans_seq = new_time_span_seq()
 
     for appointment in cd_iter_appointments(cal_day):
-        tss_plus_span(time_spans_seq, app_span(appointment))
+        app_spans_seq = tss_plus_span(app_spans_seq, app_span(appointment))
+    
+    free_time = new_time_span_seq()
+    starting_point = start
+
+    for ts in tss_iter_spans(app_spans_seq):
+        time_span_start = ts_start(ts)
+        time_span_end = ts_end(ts)
+
+        if time_precedes_or_equals(time_span_start, starting_point):
+            # The timespan is out of the time interval
+            if time_precedes(time_span_end, starting_point):
+                pass
+            # The end of timespan is within the interval 
+            if time_precedes_or_equals(time_span_end, end):
+                starting_point = time_span_end
+                pass
+            # The timespan is longer than the interval
+            if time_precedes_or_equals(end, time_span_end):
+                starting_point = time_span_end
+                break
+            if time_precedes_or_equals(starting_point, time_span_end):
+                starting_point = time_span_end
+                pass
+        
+        elif time_precedes(starting_point, time_span_start):
+            if time_precedes_or_equals(end, time_span_start):
+                free_time = tss_plus_span(free_time, new_time_span(starting_point, end))
+                starting_point = time_span_end
+                break
+            if time_precedes_or_equals(end, time_span_end):
+                free_time =tss_plus_span(free_time, new_time_span(starting_point, time_span_start))
+                starting_point = time_span_end
+                break
+            if time_precedes(time_span_end, end):
+                free_time =tss_plus_span(free_time, new_time_span(starting_point, time_span_start))
+                starting_point = time_span_end
+                pass
+    
+    
+    if time_precedes(starting_point, end):
+        free_time = tss_plus_span(free_time, new_time_span(starting_point, end))
+    
+    return free_time
+
+
+
     
     
 
